aa279156bd831b2874c7f95081f7fc7098e19097
sun5022 <sun5022@naver.com>
2024-07-23 15:28:37 +0900

    Update JoyStickMove.cs

--- JoyStickMove.cs
+++ JoyStickMove.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 
 public class JoyStickMove : MonoBehaviour
 {
+    public static JoyStickMove instance;
     public Canvas canvas;
     public Image joystickBack;
     public Image stick;
@@ -18,17 +19,18 @@ public class JoyStickMove : MonoBehaviour
 
     void Start()
     {
+        instance = this;
         backgroundRadius = joystickBack.GetComponent<RectTransform>().sizeDelta.x/2 * canvas.scaleFactor;
         stickRadius = stick.GetComponent<RectTransform>().sizeDelta.x / 2 * canvas.scaleFactor;
         joyDir = Vector3.zero;
-        print("backgroundRadius" + backgroundRadius);
+        //print("backgroundRadius" + backgroundRadius);
         joystickPos = joystickBack.transform.position;
     }
 
     public void PointerDown(BaseEventData baseEventData)
     {
         PointerEventData pointerEventData = baseEventData as PointerEventData;
-        print("PointerDown" + pointerEventData.position);
+        //print("PointerDown" + pointerEventData.position);
         firstDownPos = pointerEventData.position;
         joystickBack.transform.position = firstDownPos;
         stick.transform.position = firstDownPos;
@@ -37,7 +39,7 @@ public class JoyStickMove : MonoBehaviour
     public void PointerDrag(BaseEventData baseEventData)
     {
         PointerEventData pointerEventData = baseEventData as PointerEventData;
-        print("PointerDrag" + pointerEventData.position);
+        //print("PointerDrag" + pointerEventData.position);
         Vector2 dir = (pointerEventData.position - firstDownPos).normalized;
         joyDir = new Vector3(dir.x, 0, dir.y);
         float stickDistance = Vector3.Distance(pointerEventData.position, firstDownPos);
@@ -54,7 +56,7 @@ public class JoyStickMove : MonoBehaviour
     public void PointerUp(BaseEventData baseEventData)
     {
         PointerEventData pointerEventData = baseEventData as PointerEventData;
-        print("PointerUp" + pointerEventData.position);
+        //print("PointerUp" + pointerEventData.position);
         joystickBack.transform.position = joystickPos;
         stick.transform.position = joystickPos;
         joyDir = Vector3.zero;
